08dd0dcde85bebd9f70e01bdc1bec3587f07afa8
Andrei Stanciu <andreistanciu.ce@gmail.com>
2025-07-05 22:42:15 +0300

    fixed bug 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import "./App.css";
+import Canvas from "./graph/Canvas";
 
 function App() {
   return (
     <>
-      <h1
-        className="text-3xl font-bold underline
-        text-red-500
-      "
-      >
-        Hello world!
-      </h1>
+      <Canvas />
     </>
   );
 }
